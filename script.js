e959cc7346d8cfe040401e02795796931a4b9821
Eric Antunes <antunesv.eric@gmail.com>
2021-08-26 21:23:10 -0300

    commit initialized

---
+++ script.js
@@ -0,0 +1,20 @@
+var css = document.querySelector("h3");
+var color01 = document.querySelector(".color01");
+var color02 = document.querySelector(".color02");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background =
+	"linear-gradient(to right,"
+	+ color01.value
+	+", "
+	+ color02.value
+	+ ")";
+
+	css.textContent = body.style.background + ";"
+}
+
+color01.addEventListener("input", setGradient);
+color02.addEventListener("input", setGradient);
+
+
